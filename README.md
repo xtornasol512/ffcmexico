d4b987a968d8b55f30054fe2c710295da20e0f03
xtornasol512 <xtornasol512@gmail.com>
2014-04-03 00:17:49 -0600

    3 Abril, landingPage Basica

---
+++ README.md
@@ -0,0 +1,12 @@
+##Repositorio de FFC Mexico 
+============================
+
+#Tecnologias
+================
+#Html5
+#js
+#css3
+#Principal Web page Foundation
+#landingpage tuk tuk tapquo
+#Django Core to make more happy everything :D
+
---
+++ landingpage/package/main.js
@@ -0,0 +1,123 @@
+$(document).ready(function() {
+         $("#FFCvid").fitVids();
+         var owl = $("#owl-demo");
+ 
+          owl.owlCarousel({
+         
+              navigation : true, // Show next and prev buttons
+              slideSpeed : 300,
+              paginationSpeed : 400,
+              singleItem:true,
+              navigationText : ["Anterior", "Siguiente"]
+         
+              // "singleItem:true" is a shortcut for:
+              // items : 1, 
+              // itemsDesktop : false,
+              // itemsDesktopSmall : false,
+              // itemsTablet: false,
+              // itemsMobile : false
+         
+          });
+          $(".next").click(function(){
+            owl.trigger('owl.next');
+          })
+          $(".prev").click(function(){
+            owl.trigger('owl.prev');
+            })
+
+           
+         
+            var time = 8; // time in seconds
+ 
+  var $progressBar,
+      $bar, 
+      $elem, 
+      isPause, 
+      tick,
+      percentTime;
+ 
+    //Init the carousel
+    var owlProgramas = $("#owl-Programas");
+    owlProgramas.owlCarousel({
+      slideSpeed : 500,
+      paginationSpeed : 500,
+      items : 2,
+      itemsDesktop : [1199,2],
+      itemsDesktopSmall : [980,2],
+      itemsTablet: [768,1],
+      itemsTabletSmall: [480, 1],
+      itemsMobile : [320,1],
+      afterInit : progressBar,
+      afterMove : moved,
+      startDragging : pauseOnDragging,
+      navigation: true,
+      navigationText: [
+          "Anterior",
+          "Siguiente"
+          ]
+    });
+
+    //Init progressBar where elem is $("#owl-demo")
+    function progressBar(elem){
+      $elem = elem;
+      //build progress bar elements
+      buildProgressBar();
+      //start counting
+      start();
+    }
+ 
+    //create div#progressBar and div#bar then prepend to $("#owl-demo")
+    function buildProgressBar(){
+      $progressBar = $("<div>",{
+        id:"progressBar"
+      });
+      $bar = $("<div>",{
+        id:"bar"
+      });
+      $progressBar.append($bar).prependTo($elem);
+    }
+ 
+    function start() {
+      //reset timer
+      percentTime = 0;
+      isPause = false;
+      //run interval every 0.01 second
+      tick = setInterval(interval, 10);
+    };
+ 
+    function interval() {
+      if(isPause === false){
+        percentTime += 1 / time;
+        $bar.css({
+           width: percentTime+"%"
+         });
+        //if percentTime is equal or greater than 100
+        if(percentTime >= 100){
+          //slide to next item 
+          $elem.trigger('owl.next')
+        }
+      }
+    }
+ 
+    //pause while dragging 
+    function pauseOnDragging(){
+      isPause = true;
+    }
+ 
+    //moved callback
+    function moved(){
+      //clear interval
+      clearTimeout(tick);
+      //start again
+      start();
+    }
+ 
+    //uncomment this to make pause on mouseover 
+    // $elem.on('mouseover',function(){
+    //   isPause = true;
+    // })
+    // $elem.on('mouseout',function(){
+    //   isPause = false;
+    // })
+         
+});
