c0219caaef1ca1f3d613ca05bcf79bc49ca53014
xtornasol512 <xtornasol512@gmail.com>
2014-05-05 14:23:15 -0500

    GUlpJS, pagina 404, backend

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
+var minifyHtml = require('gulp-minify-html'); 
+
 gulp.task('scripts', function(){
     gulp.src('./temp/source/js/*.js')
         .pipe(uglify())
